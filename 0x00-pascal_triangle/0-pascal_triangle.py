b3b86ee3dc56e70e75b4cbd60e1f5b15643907f1
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-27 13:40:20 +0100

    update pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 # pascal triangle 
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]  # Initialize with the first row
+    
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        curr_row = [1]  # Start each row with 1
+        
+        for j in range(1, i):
+            curr_row.append(prev_row[j - 1] + prev_row[j])
+        
+        curr_row.append(1)  # End each row with 1
+        triangle.append(curr_row)
+    
+    return triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+# Example usage:
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    print_triangle(triangle)
