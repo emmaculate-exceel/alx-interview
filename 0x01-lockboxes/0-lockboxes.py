b5151bf465d824ddd5fb809d0e1775ff1eb5b66d
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-03 16:43:14 +0100

    update lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,26 +3,28 @@
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
     """ function that can unlock all boxes """
     n = len(boxes)
     if n == 0:
         return False
-    
+
     visited = [False] * n
     visited[0] = True  # Start with the first box unlocked
-    
+
     queue = deque([0])  # Start BFS from the first box
-    
+
     while queue:
         current_box = queue.popleft()
         for key in boxes[current_box]:
             if not visited[key]:
                 visited[key] = True
                 queue.append(key)
-    
+
     # Check if all boxes are visited
     return all(visited)
+
 
 # Example usage:
 boxes1 = [[1], [2], [3], [4], []]
