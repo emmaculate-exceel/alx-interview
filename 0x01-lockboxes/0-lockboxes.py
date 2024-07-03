636a120ceb315fb35f8d93bc49d85a1cc3b2a5b0
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-03 17:49:03 +0100

    update lockboxes; git push

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,22 +3,23 @@
 
 from collections import deque
 
+
 def canUnlockAll(boxes):
     n = len(boxes)
     if n == 0:
         return True
-    
+
     visited = [False] * n
     visited[0] = True
     queue = deque([0])
-    
+
     while queue:
         current_box = queue.popleft()
         keys = boxes[current_box]
-        
+
         for key in keys:
             if key < n and not visited[key]:
                 visited[key] = True
                 queue.append(key)
-    
+
     return all(visited)
