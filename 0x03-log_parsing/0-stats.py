48cc7a83a16b492b31e866c2642e1a54ad4348c5
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-19 03:49:23 +0100

    print statistic

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -27,5 +27,31 @@ def handle_sig(sig, interface):
     """ handling signal interuption """
     print_statistic()
     sys.exit(0)
+
+
 signal.signal(signal.SIGINT, handle_sig)
 
+def parse_line(line):
+    """ parsing the line of """
+    global status_code, line_count, file_size
+    line_count = +1
+    pattern = r'^\S+ - \[[^\]]+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)'
+
+    match = re.match(pattern, line)
+    if match:
+        status_code = int(match.group(1))
+        file_s = int(match.group(2))
+
+        if status in status_code:
+            status_code[status] += 1
+            
+        file_size += file_s
+
+
+def print_statistic():
+    """ this function print sys.stdin line statistic """
+    print(f"Total file size: {total_size}")
+    for code in sorted(status_code.keys()):
+        if status_code[code] > 0:
+            print(f"{code}: {status_code[code]}")
+            
