2af394409563a6dcb90650acc93c665e73c6f4ab
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-19 05:51:14 +0100

    print

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict, file_size: int) -> None:
+    def print_stats(stats: dict, file_size: int):
         print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
             if v:
