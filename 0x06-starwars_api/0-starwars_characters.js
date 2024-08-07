40a83597638da357570f83f6cf36458bd78ea159
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-07 16:04:55 +0100

    update_startwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,17 +6,17 @@ const request = require('request');
 const movieId = process.argv[2];
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function sendReq (character_name, index) {
-  if (character_name.length === index) {
+function sendReq (characterName, index) {
+  if (characterName.length === index) {
     return;
   }
 
-  request(character_name[index], (error, response, body) => {
+  request(characterName[index], (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
       console.log(JSON.parse(body).name);
-      sendReq(character_name, index + 1);
+      sendReq(characterName, index + 1);
     }
   });
 }
@@ -25,8 +25,8 @@ request(movieEndpoint, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const character_name = JSON.parse(body).characters;
+    const characterName = JSON.parse(body).characters;
 
-    sendReq(character_name, 0);
+    sendReq(characterName, 0);
   }
 });
