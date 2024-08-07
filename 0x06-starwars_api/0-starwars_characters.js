8c9e257ab3002d84147d39692529c1063725de1a
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-07 16:01:32 +0100

    update_startwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,32 @@
 #!/usr/bin/node
-// starwars api using nodejs
+// star wars api using nodejs
 
 const request = require('request');
-const process = require('process');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
 
 const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+function sendReq (character_name, index) {
+  if (character_name.length === index) {
     return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch movie details. Status code:', response.statusCode);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error('Failed to fetch character details. Status code:', response.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
+  request(character_name[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendReq(character_name, index + 1);
+    }
   });
+}
+
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const character_name = JSON.parse(body).characters;
+
+    sendReq(character_name, 0);
+  }
 });
