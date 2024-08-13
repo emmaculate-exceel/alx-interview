5840071b3708045749dd51aa53d0e0a042bdd123
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-13 11:21:36 +0100

    2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """ creating a 2d matrix """
 
+
 def rotate_2d_matrix(matrix):
     """
     a function to rotate a 2d matrix
     """
-    
+
     n = len(matrix)
-    
+
     # Step 1: Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     # Step 2: Reverse each row
     for i in range(n):
         matrix[i].reverse()
+
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
