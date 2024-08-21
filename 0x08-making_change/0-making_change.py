3ea9300a4f174f2408b62ca99d2b241f8152268a
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-21 15:49:23 +0100

    making files

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""making changes to file using python
+"""
+
+def makeChange(coins, total):
+    """ coins and it's sum
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+if __name__ == "__main__":
+    coins = [1, 2, 3]
+    total = 11
+    print(makeChange(coins, total))
